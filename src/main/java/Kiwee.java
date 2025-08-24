42954a5ebc6a70f77ca967cc520f380063792c70
Foo Kang <fookang3@gmail.com>
2025-08-24 13:18:15 +0800

    Add echo and exit on 'bye' command

--- src/main/java/Kiwee.java
+++ src/main/java/Kiwee.java
@@ -1,12 +1,34 @@
+import java.util.Scanner;
+
 public class Kiwee {
     public static void main(String[] args) {
-        String logo = "____________________________________________________________\n"
-                + " Hello! I'm Kiwee\n"
-                + " What can I do for you?\n"
-                + "____________________________________________________________\n"
-                + " Bye. Hope to see you again soon!\n"
-                + "____________________________________________________________\n";
+        final String PARTITION = "____________________________________________________________";
 
-        System.out.println(logo);
+        final String LOGO = PARTITION + "\n"
+                + " Hello! I'm Kiwee \uD83E\uDD5D \n"
+                + " How can I help you?\n"
+                + PARTITION;
+
+        final String BYE_MESSAGE = PARTITION + "\n"
+                + " Bye \uD83D\uDC4B Hope to see you again soon!\n"
+                + PARTITION;
+
+        Scanner input = new Scanner(System.in);
+        System.out.println(LOGO);
+
+        while (true) {
+            String userInput = input.nextLine().trim();
+
+            if (userInput.equalsIgnoreCase("bye")) {
+                System.out.println(BYE_MESSAGE);
+                break;
+            } else if (!userInput.isEmpty()) {
+                String result = PARTITION + "\n"
+                        + " " + userInput + "\n"
+                        + PARTITION;
+                System.out.println(result);
+            }
+            // else: ignore blank line
+        }
     }
 }
